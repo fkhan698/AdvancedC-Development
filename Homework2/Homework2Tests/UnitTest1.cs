d8963be7d0732ee7de8d25bf0863f7e079471e53
Faizan Khan <fkhan698@hotmail.com>
2021-02-14 14:53:15 -0600

    Added new unit tests

--- Homework2/Homework2Tests/UnitTest1.cs
+++ Homework2/Homework2Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
-using Homework2;
+using Homework2;                     
+
+
 using Microsoft.VisualStudio.TestPlatform.TestHost;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
@@ -56,7 +58,8 @@ namespace Homework2Tests
 
 
             //Act
-            taxableItems.Add(new TShirt { Price= 10, ShippingRate =  1, Size = "Small", TaxRate = 0.06D});
+            taxableItems.Add(new  Book{ Price= 20, ShippingRate =  4, TaxRate = 0.06D});
+            taxableItems.Add(new TShirt { Price = 5, ShippingRate = 2, TaxRate = 0.07D });
 
             //Assert 
            CollectionAssert.AllItemsAreInstancesOfType(taxableItems, typeof(ITaxable));
@@ -71,6 +74,24 @@ namespace Homework2Tests
             //Act
             shippableItems.Add(new TShirt { Price = 10, ShippingRate = 1, Size = "Small", TaxRate = 0.06D });
             //Assert 
+            CollectionAssert.AllItemsAreInstancesOfType(shippableItems, typeof(IShippable));
+        }
+        #endregion
+        #region CheckPurchaseableItems
+        [TestMethod]
+        public void CheckPurchaseAbleItems()
+        {
+            //Arrange
+            var purchaseableItems = new List<IPurchasable>();
+
+            //Act
+            purchaseableItems.Add(new Snack { Price = 2 });
+            purchaseableItems.Add(new TShirt { Price = 10, ShippingRate = 1, Size = "Small", TaxRate = 0.06D });
+
+            //Assert
+            CollectionAssert.AllItemsAreInstancesOfType(purchaseableItems, typeof(IPurchasable));
+
+
         }
         #endregion
         #region CheckGrandTotal
@@ -89,19 +110,69 @@ namespace Homework2Tests
             Assert.AreEqual(grandTotal, 4.50);
         }
         #endregion
-
-        #region CheckTaxRate
+        #region CheckAreNotSame
         [TestMethod]
-        public void CheckTaxRate()
+        public void CheckIfNotSame()
         {
             //Arrange
             var book = new Book();
             var shirt = new TShirt();
-
+            //Assert
             Assert.AreNotSame(book, shirt);
 
             
         }
         #endregion
+        #region CheckTaxDouble
+        [TestMethod] 
+        public void TaxDouble()
+        {
+            //Arrange
+            var taxableItems = new List<ITaxable> {
+               new Book {Title = "Clean Code", Price = 15D, ShippingRate = 2, TaxRate = 0.07D},
+               new TShirt{Price = 5, ShippingRate = 3, TaxRate = 0.08D}
+            };
+
+            var tax = Homework2.Program.CalculateTax(taxableItems);
+
+            Assert.IsInstanceOfType(tax, typeof(double));
+
+        }
+        #endregion
+        #region CheckShipRateDouble
+        [TestMethod]
+        public void ShipRateDouble()
+        {
+            //Arrange
+            var shippableItems = new List<IShippable> {
+               new Book {Title = "Clean Code", Price = 15D, ShippingRate = 2, TaxRate = 0.07D},
+               new TShirt{Price = 5, ShippingRate = 3, TaxRate = 0.08D}
+            };
+
+            var ship = Homework2.Program.CalculateShipping(shippableItems);
+
+            Assert.IsInstanceOfType(ship, typeof(double));
+
+        }
+        #endregion
+        #region CheckTitleString
+        [TestMethod]
+        public void CheckTitleString()
+        {
+            var book = new Book()
+            {
+                Title = "Welcome to Advanced C#"
+            };
+
+            var title = book.Title;
+
+            Assert.IsInstanceOfType(title, typeof(string));
+
+
+        }
+        #endregion  
+
+
+
     }
 }
